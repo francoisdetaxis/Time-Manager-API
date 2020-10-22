80e27bda42b9e0d04ac102ff887fcae5b09f44c8
François de taxis du Poët <francois.de-taxis-du-poet@epitech.eu>
2020-10-22 10:36:11 +0200

    CORS fix

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule AppTest.MixProject do
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
+      {:cors_plug, "~> 1.5"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.2.0"},
       {:telemetry_metrics, "~> 0.4"},
