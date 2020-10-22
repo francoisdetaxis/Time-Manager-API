3c7399a7610c53b2812b777f88cbbac9f9e6ac8c
Alexandre Garcia <alexandre.garcia-accar@epitech.eu>
2020-10-22 16:38:25 +0200

    toto

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :app_test, AppTest.Repo,
 # with webpack to recompile .js and .css sources.
 config :app_test, AppTestWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: false,
+  debug_errors: true,
   code_reloader: true,
   check_origin: false,
   watchers: [
