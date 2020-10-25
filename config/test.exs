b0936a015f6e18a50db45a567074ce798423b08b
Unknown <orencohen2652@gmail.com>
2020-10-25 13:49:51 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ config/test.exs
@@ -0,0 +1,23 @@
+use Mix.Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :app_test, AppTest.Repo,
+  username: "postgres",
+  password: "chatriki13",
+#  database: "app_test_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "time_manager",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :app_test, AppTestWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
